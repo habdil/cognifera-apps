c6ecc3c4cc0813dea290a6964fac8f7067cf5869
habdil <habdiliqrawardana@gmail.com>
2025-08-28 18:04:19 +0700

    Update metadata

--- app/journal/page.tsx
+++ app/journal/page.tsx
@@ -2,8 +2,8 @@ import { Metadata } from "next";
 import JournalLayout from "@/components/journal/JournalLayout";
 
 export const metadata: Metadata = {
-  title: "Journal Pendidikan IPA Indonesia | Cognifera",
-  description: "Indonesian Journal of Science Education - Platform publikasi ilmiah untuk pendidikan IPA Indonesia",
+  title: "Journal Penelitian Cognifera | Cognifera",
+  description: "Journal Penelitian Cognifera - Platform publikasi ilmiah untuk pendidikan Indonesia",
 };
 
 export default function JournalPage() {
